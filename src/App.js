955fcc37d2ba3f8fec1a521e5d919263c55d2c50
vfg-95 <sam.ridding@hotmail.co.uk>
2022-08-22 16:28:23 +0000

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hiya</h1>
+      <NavBar />
     </div>
   );
 }
