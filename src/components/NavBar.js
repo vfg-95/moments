15c950db7b7c2f02455a496b7c6ec3551912d596
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-04 13:48:35 +0000

    Add custom hook for navbar burger toggle

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,18 +9,21 @@ import {
 } from "../contexts/CurrentUserContexts";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
 
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
+
   const handleSignOut = async () => {
     try {
-      await axios.post("/dj-rest-auth/logout");
-      setCurrentUser(null)
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
     } catch (err) {
-      console.log(err)
-    }  
+      console.log(err);
+    }
   };
 
   const addPostIcon = (
@@ -79,7 +82,12 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -87,7 +95,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
